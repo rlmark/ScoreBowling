156f7f156f2a273a9777d8ba937a9476e1140811
Rebecca Mark <rebecca.lmark@gmail.com>
2018-03-01 01:29:12 -0800

    Refactored Frame to keep a list of balls instead of case class for each

--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -1,30 +1,26 @@
 sealed trait Frame {
-  val ball1: Int
-  val ball2: Int
-  val ball3: Int
-
-//  val balls: List[Int]
-  val isComplete: Boolean
+  val balls: List[Int]
+  val complete: Boolean
   val score: Option[Int]  // TODO use frameStatus
 }
 
-case class OpenFrame(ball1: Int, ball2: Int, ball3:Int, isComplete: Boolean, score: Option[Int]) extends Frame
-case class Spare(ball1: Int, ball2: Int, ball3:Int, isComplete: Boolean, score: Option[Int]) extends Frame
-case class Strike(ball1: Int, ball2: Int, ball3:Int, isComplete: Boolean, score: Option[Int]) extends Frame
+case class OpenFrame(balls: List[Int], complete: Boolean, score: Option[Int]) extends Frame
+case class Spare(balls: List[Int], complete: Boolean, score: Option[Int]) extends Frame
+case class Strike(balls: List[Int], complete: Boolean, score: Option[Int]) extends Frame
 
 object Frame {
   def apply(turn: List[Int]): Frame = {
     turn match {
-      case firstBall :: secondBall :: Nil if firstBall + secondBall < 10 =>
-        OpenFrame(firstBall, secondBall, -1, isComplete = true, None)
-      case firstBall :: secondBall :: Nil if firstBall + secondBall == 10 =>
-        Spare(firstBall, secondBall, -1, isComplete = false, None)
-      case firstBall :: Nil if firstBall == 10 =>
-        Strike( 10, -1, -1, isComplete = false, None)
-      case firstBall :: secondBall:: thirdBall :: Nil if firstBall == 10 =>
-        Strike(firstBall, secondBall, thirdBall, isComplete = true, None)
-      case firstBall :: secondBall:: thirdBall :: Nil if firstBall + secondBall == 10 =>
-        Spare(firstBall, secondBall, thirdBall, isComplete = true, None)
+      case t@firstBall :: secondBall :: Nil if firstBall + secondBall < 10 =>
+        OpenFrame(t, complete = true, None)
+      case t@firstBall :: secondBall :: Nil if firstBall + secondBall == 10 =>
+        Spare(t, complete = false, None)
+      case t@firstBall :: Nil if firstBall == 10 =>
+        Strike( t, complete = false, None)
+      case t@firstBall :: secondBall:: thirdBall :: Nil if firstBall == 10 =>
+        Strike(t, complete = true, None)
+      case t@firstBall :: secondBall:: thirdBall :: Nil if firstBall + secondBall == 10 =>
+        Spare(t, complete = true, None)
     }
   }
 
--- src/main/scala/FrameStatus.scala
+++ src/main/scala/FrameStatus.scala
@@ -4,11 +4,6 @@ case class Score(curentFrame: Frame, scoreValue: Int) extends FrameStatus
 
 object FrameStatus{
   def apply(frame: Frame): FrameStatus = {
-    frame match {
-      case f@OpenFrame(b1, b2, _, true, _) => Score(f, b1 + b2)
-      case f@Spare(b1, b2, b3, true, _) => Score(f, b1 + b2 + b3)
-      case f@Strike(b1, b2, b3, true, _) => Score(f, b1 + b2 + b3)
-      case f@_ => Pending(f)
-    }
+    if (frame.complete) Score(frame, frame.balls.sum) else Pending(frame)
   }
 }
