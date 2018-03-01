82fe58bb84bccb2648b26469d7e39bf9fa51d953
Rebecca Mark <rebecca.lmark@gmail.com>
2018-03-01 01:55:45 -0800

    Removed extra score value from frame

--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -1,38 +1,37 @@
 sealed trait Frame {
   val balls: List[Int]
   val complete: Boolean
-  val score: Option[Int]  // TODO use frameStatus
 }
 
-case class OpenFrame(balls: List[Int], complete: Boolean, score: Option[Int]) extends Frame
-case class Spare(balls: List[Int], complete: Boolean, score: Option[Int]) extends Frame
-case class Strike(balls: List[Int], complete: Boolean, score: Option[Int]) extends Frame
+case class OpenFrame(balls: List[Int], complete: Boolean) extends Frame
+case class Spare(balls: List[Int], complete: Boolean) extends Frame
+case class Strike(balls: List[Int], complete: Boolean) extends Frame
 
 object Frame {
   def apply(turn: List[Int]): Frame = {
     turn match {
       case t@firstBall :: secondBall :: Nil if firstBall + secondBall < 10 =>
-        OpenFrame(t, complete = true, None)
+        OpenFrame(t, complete = true )
       case t@firstBall :: secondBall :: Nil if firstBall + secondBall == 10 =>
-        Spare(t, complete = false, None)
+        Spare(t, complete = false)
       case t@firstBall :: Nil if firstBall == 10 =>
-        Strike( t, complete = false, None)
+        Strike( t, complete = false)
       case t@firstBall :: secondBall:: thirdBall :: Nil if firstBall == 10 =>
-        Strike(t, complete = true, None)
+        Strike(t, complete = true)
       case t@firstBall :: secondBall:: thirdBall :: Nil if firstBall + secondBall == 10 =>
-        Spare(t, complete = true, None)
+        Spare(t, complete = true)
     }
   }
 
   def updateFrame(turn: List[Int], previousFrame: Frame): Frame = {
     previousFrame match {
-      case Spare(bs, _, _) =>
-        Spare(bs :+ turn.head, complete = true, None) // if the previous frame was a spare, get the first roll from turn
-      case Strike(bs, _, _) if turn.lengthCompare(1) =>
-        Strike(bs ++ turn, complete = false, None) // Two strikes in a row
-      case Strike(bs, _, _) =>
+      case Spare(bs, _) =>
+        Spare(bs :+ turn.head, complete = true) // if the previous frame was a spare, get the first roll from turn
+      case Strike(bs, _) if turn.lengthCompare(1) =>
+        Strike(bs ++ turn, complete = false) // Two strikes in a row
+      case Strike(bs, _) =>
         val totalBalls = bs ++ turn
-        Strike(totalBalls.slice(0, 4), complete = true, None) // takes care of (10, 10, 4) and (10, 4, 2) case
+        Strike(totalBalls.slice(0, 4), complete = true) // takes care of (10, 10, 4) and (10, 4, 2) case
     }
   }
 }
